97095a79a23799d3ded3c85ef771000bdfb56c56
AEZakmee <73796697+aezakmee@users.noreply.github.com>
2025-03-12 22:10:54 +0200

    Upgrade to latest ios/android for flutter 3.29

--- app/lib/di/dependencies/hive.dart
+++ app/lib/di/dependencies/hive.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/foundation.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:presentation/app/di/locator.dart';
 
 import '../../config/hive_boxes.dart';
@@ -6,7 +8,10 @@ import '../../config/hive_boxes.dart';
 Future<void> setupHive() async {
   await Hive.initFlutter();
 
-  final collection = await BoxCollection.open('AppDB', {HiveBoxes.generic});
+  final directory = !kIsWeb ? await getApplicationDocumentsDirectory() : null;
+  final collection = await BoxCollection.open('AppDB', {
+    HiveBoxes.generic,
+  }, path: directory?.path);
 
   final genericBox = await collection.openBox(HiveBoxes.generic);
 
